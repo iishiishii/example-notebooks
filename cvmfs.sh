7cde8099f7bc61d07c207c3898001a0f4f609213
iishiishii <love_sweet_98@yahoo.com.vn>
2023-06-17 00:22:28 +1000

    setup cvmfs

---
+++ cvmfs.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env bash
+set -e
+
+#setup singularity 2.6.1 from neurodebian
+# This is for 18.04
+# wget -O- http://neuro.debian.net/lists/bionic.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+# sudo apt-key adv --recv-keys --keyserver hkp://pool.sks-keyservers.net:80 0xA5D32F012649A5A9
+
+# This is for 20.04 (singularity-container is not build for later versions in neuro debian project yet)
+wget -O- http://neuro.debian.net/lists/focal.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
+
+# This is for 22.04 (but no singularity-container package available)
+# wget -O- http://neuro.debian.net/lists/jammy.us-nh.full | sudo tee /etc/apt/sources.list.d/neurodebian.sources.list
+# sudo apt-key adv --recv-keys --keyserver hkps://keyserver.ubuntu.com 0xA5D32F012649A5A9
+
+sudo apt-get update
+sudo apt-get remove aria2
+sudo apt-get install singularity-container 
+echo "checking if CVMFS part works:"
+
+sudo apt-get install lsb-release
+wget https://ecsft.cern.ch/dist/cvmfs/cvmfs-release/cvmfs-release-latest_all.deb
+
+echo "[DEBUG]: adding cfms repo"
+sudo dpkg -i cvmfs-release-latest_all.deb
+echo "[DEBUG]: apt-get update"
+sudo apt-get update --allow-unauthenticated
+echo "[DEBUG]: apt-get install cvmfs"
+sudo apt-get install cvmfs --allow-unauthenticated
+
+sudo mkdir -p /etc/cvmfs/keys/ardc.edu.au/
+
+echo "-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwUPEmxDp217SAtZxaBep
+Bi2TQcLoh5AJ//HSIz68ypjOGFjwExGlHb95Frhu1SpcH5OASbV+jJ60oEBLi3sD
+qA6rGYt9kVi90lWvEjQnhBkPb0uWcp1gNqQAUocybCzHvoiG3fUzAe259CrK09qR
+pX8sZhgK3eHlfx4ycyMiIQeg66AHlgVCJ2fKa6fl1vnh6adJEPULmn6vZnevvUke
+I6U1VcYTKm5dPMrOlY/fGimKlyWvivzVv1laa5TAR2Dt4CfdQncOz+rkXmWjLjkD
+87WMiTgtKybsmMLb2yCGSgLSArlSWhbMA0MaZSzAwE9PJKCCMvTANo5644zc8jBe
+NQIDAQAB
+-----END PUBLIC KEY-----" | sudo tee /etc/cvmfs/keys/ardc.edu.au/neurodesk.ardc.edu.au.pub
+
+
+echo "CVMFS_USE_GEOAPI=yes" | sudo tee /etc/cvmfs/config.d/neurodesk.ardc.edu.au.conf
+
+echo 'CVMFS_SERVER_URL="http://cvmfs.neurodesk.org/cvmfs/@fqrn@;http://cvmfs-brisbane.neurodesk.org/cvmfs/@fqrn@;http://cvmfs-sydney.neurodesk.org/cvmfs/@fqrn@;http://cvmfs-frankfurt.neurodesk.org/cvmfs/@fqrn@;http://cvmfs-zurich.neurodesk.org/cvmfs/@fqrn@;http://cvmfs-toronto.neurodesk.org/cvmfs/@fqrn@;http://cvmfs-ashburn.neurodesk.org/cvmfs/@fqrn@;http://cvmfs.neurodesk.org/cvmfs/@fqrn@"' | sudo tee -a /etc/cvmfs/config.d/neurodesk.ardc.edu.au.conf 
+
+echo 'CVMFS_KEYS_DIR="/etc/cvmfs/keys/ardc.edu.au/"' | sudo tee -a /etc/cvmfs/config.d/neurodesk.ardc.edu.au.conf
+
+echo "CVMFS_HTTP_PROXY=DIRECT" | sudo tee  /etc/cvmfs/default.local
+echo "CVMFS_QUOTA_LIMIT=5000" | sudo tee -a  /etc/cvmfs/default.local
+
+sudo cvmfs_config setup
+sudo cvmfs_config chksetup
+
+ls /cvmfs/neurodesk.ardc.edu.au/containers
+
+cvmfs_config stat -v neurodesk.ardc.edu.au
+
+bash run_transparent_singularity.sh --container itksnap_3.8.0_20201208.simg
+
+# check if container exists on cvmfs
+
+if [[ -d "/cvmfs/neurodesk.ardc.edu.au/containers/itksnap_3.8.0_20201208" ]]; then
+   echo "Container exists in cvmfs"
+else
+   echo "Container does NOT exist!"
+   exit 1
+fi
+
+# check if container link exists
+if [ -L /home/runner/work/transparent-singularity/transparent-singularity/itksnap_3.8.0_20201208.simg ]; then
+    echo "[DEBUG]: Container file link exists"
+else 
+    echo "[DEBUG]: Container file does not exist! Something went wrong."
+    exit 1
+fi
+
+# check if transparent singularity generated executable output file:
+FILE="/home/runner/work/transparent-singularity/transparent-singularity/itksnap"
+if [ -f $FILE ];then
+    echo "[DEBUG]: $FILE exists."
+else
+    echo "[DEBUG]: $FILE doesn't exist. Something went wrong with transparent singularity. "
+    exit 1
+fi
