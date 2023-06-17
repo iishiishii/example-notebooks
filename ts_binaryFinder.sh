697016a864114eab7376fab3fdb9d7ef017d4b13
iishiishii <love_sweet_98@yahoo.com.vn>
2023-06-17 23:20:07 +1000

    setup cvmfs

---
+++ ts_binaryFinder.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+IFS=':'; \
+for i in $DEPLOY_PATH; \
+   do find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done > commands_raw.txt
+
+for i in $DEPLOY_BINS; \
+   do echo "$i"; done >> commands_raw.txt
+
+# if $DEBUG
+#    export DEPLOY_ENV_FSLDIR="BASEPATH/opt/fsl-5.0.2"
+#    export DEPLOY_ENV_SPMMCRCMD="BASEPATH/opt/spm12/run_spm12.sh BASEPATH/opt/mcr/v97/ script"
+# fi
+
+
+# Find system applications in Neurodesktop:
+# IFS=':'; \
+# for i in $PATH; \
+#    do find "$i" -maxdepth 1 -executable -type f -exec basename {} \;; done > system_commands_raw.txt
+
+# Find binaries in FSL image:
+# comm -12 <( sort commands_raw.txt ) <( sort system_commands_raw.txt ) > overlap.txt
+
+
+# Remove system applications from commands.txt, because they cause problems:
+getListOfSystemCommandsToBeDeleted() {
+  printf '%s\n' `cat ts_binaryFinderExcludes.txt`
+}
+
+sed -E 's/\<('"$(tr '\n' '|' < <(getListOfSystemCommandsToBeDeleted) )"')\>//gI' < commands_raw.txt > commands.txt
+sed -i '/^\s*$/d' commands.txt
+
+touch env.txt
+env | grep DEPLOY_ENV_ > env.txt
