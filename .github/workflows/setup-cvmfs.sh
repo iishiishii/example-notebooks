7c8cd7fffa3eccae172d847cf16edbdd448d04fe
iishiishii <love_sweet_98@yahoo.com.vn>
2023-06-20 15:29:19 +1000

    use subprocess to run bash commands in python

--- .github/workflows/setup-cvmfs.sh
+++ .github/workflows/setup-cvmfs.sh
@@ -46,8 +46,7 @@ sudo cvmfs_config setup
 id cvmfs
 
 # Disabling autofs is needed, otherwise autofs is not fast enough to mount CVMFS and it will complain about it with "too many symbolic errors"
-sudo mkdir /cvmfs
-sudo mkdir /cvmfs/neurodesk.ardc.edu.au
+sudo mkdir -p /cvmfs/neurodesk.ardc.edu.au
 sudo mount -t cvmfs neurodesk.ardc.edu.au /cvmfs/neurodesk.ardc.edu.au
 
 sudo cvmfs_config chksetup
